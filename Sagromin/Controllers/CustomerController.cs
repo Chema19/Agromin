3551cf51253b1678377cdce008817f5f842225fc
josemaria inga villafuerte <chemalonso96@gmail.com>
2019-05-10 23:43:24 -0500

    modulo local

--- Sagromin/Controllers/CustomerController.cs
+++ Sagromin/Controllers/CustomerController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Sagromin.ViewModels.Customer;
 using System.Transactions;
 using Sagromin.Models;
+using System.Text.RegularExpressions;
 using Sagromin.Helpers;
 
 namespace Sagromin.Controllers
@@ -32,6 +33,13 @@ namespace Sagromin.Controllers
         {
             try
             {
+
+                if (!email_bien_escrito(model.Email))
+                {
+                    PostMessage(MessageType.Error, "Formato email incorrecto");
+                    return RedirectToAction("ListCustomer");
+                }
+
                 using (var ts = new TransactionScope())
                 {
 
@@ -93,6 +101,27 @@ namespace Sagromin.Controllers
             catch (Exception e)
             {
                 return Json(new { });
+            }
+        }
+
+        private Boolean email_bien_escrito(String email)
+        {
+            String expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
             }
         }
     }
